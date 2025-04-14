8206e36e9f17e44511cfbf8ece0f405ef7463409
NicoLedesmaUade <nledesma@uade.edu.ar>
2025-04-14 18:46:18 -0300

    funciones pedir pregunta y leer txt

--- funciones.py
+++ funciones.py
@@ -11,3 +11,23 @@ def agregar_pregunta(pregunta, categoria,dificultad,respuesta):
         archivo.write(f'{pregunta};{categoria};{dificultad};{respuesta}\n')
     print("Pregunta agregada con éxito.")
 
+def pedir_pregunta():
+    pregunta = input("Ingrese la pregunta: ")
+    categoria = input("Ingrese la categoria: ")
+    dificultad = input("Ingrese la dificultad: ")
+    respuesta = input("Ingrese la respuesta: ")
+    agregar_pregunta(pregunta, categoria, dificultad, respuesta)
+
+def leer_preguntas():
+    preguntas = []
+    with open('preguntas.txt', 'r') as archivo:
+        for linea in archivo:
+            datos = linea.strip().split(';')
+            preguntas.append(datos)  # Cada pregunta es una lista con sus datos
+    print("Preguntas cargadas exitosamente",preguntas)
+    return preguntas
+
+crear_archivo()
+pedir_pregunta()
+pedir_pregunta()
+leer_preguntas()
