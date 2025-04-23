3b90cc3b52df8fba4081b3190e2ca0ebe8d7db19
lgagliani <lgagliani@ssn.gob.ar>
2025-04-23 14:39:04 -0300

    Agrego reglas del juego

--- menu.py
+++ menu.py
@@ -2,4 +2,5 @@ def mostrarMenu():
     print("1. Modo 1vs1")
     print("2. Modo por tiempo")
     print("3. Ver ranking")
-    print("4. Salir")
+    print("4. Reglas del juego")
+    print("5. Salir")
