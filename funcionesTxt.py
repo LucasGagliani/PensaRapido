4416d7ac1dd3321fb751b70c9e6de2642a0d2376
NicoLedesmaUade <nledesma@uade.edu.ar>
2025-04-27 19:09:16 -0300

    agregando comentarios

--- funcionesTxt.py
+++ funcionesTxt.py
@@ -1,11 +1,17 @@
+# preguntasTupla es una tupla que contiene tres listas, cada una representando un nivel de dificultad:
+# fácil, medio y difícil. Dentro de cada nivel hay cinco listas vacías que sirven como categorías 
+# iniciales. Estas listas pueden ser utilizadas para almacenar preguntas y respuestas organizadas por nivel y categoría.
 preguntasTupla = (
-    [[] for i in range(5)],
-    [[] for i in range(5)],
-    [[] for i in range(5)]
+    [[] for i in range(5)],  # Nivel de dificultad 1 (fácil)
+    [[] for i in range(5)],  # Nivel de dificultad 2 (medio)
+    [[] for i in range(5)]   # Nivel de dificultad 3 (difícil)
 )
 
 
+
 def leerPreguntas():
+#Lee el archivo 'preguntas.txt' y organiza las preguntas en 'preguntasTupla' 
+#según nivel de dificultad y categoría.
     with open('preguntas.txt', 'r', encoding='utf-8') as archivo:
         for numero_linea, linea in enumerate(archivo, start=1):
             datos = linea.strip().split(';')
